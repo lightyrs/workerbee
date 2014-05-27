ff22c29c8e9ef8f238c21612776443a49156e8c4
Harris Novick <harrisdavidnovick@gmail.com>
2014-05-27 17:36:16 -0400

    run brew update at 5:30 pm daily

--- config/schedule.rb
+++ config/schedule.rb
@@ -1,20 +1,9 @@
-# Use this file to easily define all of your cron jobs.
-#
-# It's helpful, but not entirely necessary to understand cron before proceeding.
-# http://en.wikipedia.org/wiki/Cron
+# https://github.com/javan/whenever
 
-# Example:
-#
-# set :output, "/path/to/my/cron_log.log"
-#
-# every 2.hours do
-#   command "/usr/bin/some_great_command"
-#   runner "MyModel.some_method"
-#   rake "some:great:rake:task"
-# end
-#
-# every 4.days do
-#   runner "AnotherModel.prune_old_records"
-# end
+set :output, "/Users/harris/workspace/workerbee/logs/whenever.log"
 
-# Learn more: http://github.com/javan/whenever
+# Update Homebrew
+
+every 1.day, at: '5:30 pm' do
+  command "/usr/local/bin/brew update"
+end
