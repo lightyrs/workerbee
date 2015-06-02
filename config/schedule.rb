a2db27ce3ee0c587ca582aed570479d53406c20d
Harris Novick <harrisdavidnovick@gmail.com>
2015-06-01 21:56:43 -0400

    added stackoverflow task

--- config/schedule.rb
+++ config/schedule.rb
@@ -3,7 +3,11 @@
 set :output, "/Users/harris/workspace/workerbee/logs/whenever.log"
 
 # Update Homebrew
-
 every 1.day, at: '5:30 pm' do
   command "/usr/local/bin/brew update"
+end
+
+# Visit Stack Overflow
+every 1.day, at: '5:30 am' do
+  command "launchy http://stackoverflow.com/users/111363/lightyrs"
 end
